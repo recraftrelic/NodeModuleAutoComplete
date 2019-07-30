67430d8fec7e86c3ae98c22026d7f518069dd630
Manoj Singh Negi <justanothermanoj@gmail.com>
2019-07-31 02:27:37 +0530

    parse and return dependencies auto completions

--- NodeModulesAutoComplete.py
+++ NodeModulesAutoComplete.py
@@ -1,6 +1,19 @@
+import os.path
 import sublime
 import sublime_plugin
+import json
+from os import path
 
 class NodeModulesAutoCompleteCommand(sublime_plugin.EventListener):
 	def on_query_completions(self, view, prefix, locations):
-		return ["sublime", "sublime text", "sublime text 3"]
+		project_variables = sublime.active_window().extract_variables()
+		project_folder = project_variables['folder']
+		if project_folder is not None:
+			package_json_path = project_folder + "/package.json"
+			if path.exists(package_json_path):
+				dependenciesArr = []
+				with open(package_json_path) as package_json:
+					data = json.load(package_json)
+					for dependency in data["dependencies"]:
+						dependenciesArr.append([dependency, dependency])
+				return (dependenciesArr, 0)
